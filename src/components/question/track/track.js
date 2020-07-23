f2fc62bcfac634d646bdc0cbe6474eea58485321
Goreev <goreev.ar@gmailc.om>
2020-07-23 12:47:53 +0300

    equalizer

--- src/components/question/track/track.js
+++ src/components/question/track/track.js
@@ -4,68 +4,77 @@ import "react-h5-audio-player/lib/styles.css";
 import classes from "./track.module.css";
 import AudioSpectrum from "react-audio-spectrum";
 import { isSafari, isMobile } from "react-device-detect";
+import equalizer from "./equalizer.svg";
 
 class Track extends Component {
-  state = {
-    audioCanPlay: false,
-    autoPlay: false,
-  };
-  //must play on track prop changes
-  componentDidUpdate(prevProps) {
-    if (this.props.track !== prevProps.track) {
-      this.player.current.audio.current.play();
+    state = {
+        audioCanPlay: false,
+        autoPlay: false,
+    };
+    //must play on track prop changes
+    componentDidUpdate(prevProps) {
+        if (this.props.track !== prevProps.track) {
+            this.player.current.audio.current.play();
+        }
     }
-  }
-  player = createRef();
+    player = createRef();
 
-  onAudioCanPlay = () => {
-    this.setState({
-      audioCanPlay: true,
-    });
-  };
+    onAudioCanPlay = () => {
+        this.setState({
+            audioCanPlay: true,
+        });
+    };
 
-  render() {
-    const { track, onTrackEnded } = this.props;
+    render() {
+        const { track, onTrackEnded } = this.props;
 
-    let vizualizer = <div className={classes.Vizualizer}></div>;
+        let vizualizer = <div className={classes.Vizualizer}></div>;
 
-    if (this.state.audioCanPlay && (!isSafari || isMobile)) {
-      vizualizer = (
-        <div className={classes.Vizualizer}>
-          <AudioSpectrum
-            id="audio-canvas"
-            height={100}
-            width={300}
-            audioEle={this.player.current.audio.current}
-            capColor={"#FF2A73"}
-            capHeight={2}
-            meterWidth={10}
-            meterCount={512}
-            meterColor={[
-              { stop: 0, color: "#3D58F4" },
-              { stop: 0.5, color: "#883CD0" },
-              { stop: 1, color: "#D924AC" },
-            ]}
-            gap={4}
-          />
-        </div>
-      );
+        if (this.state.audioCanPlay) {
+            if (!isSafari || isMobile) {
+                vizualizer = (
+                    <div className={classes.Vizualizer}>
+                        <AudioSpectrum
+                            id="audio-canvas"
+                            height={100}
+                            width={300}
+                            audioEle={this.player.current.audio.current}
+                            capColor={"#FF2A73"}
+                            capHeight={2}
+                            meterWidth={10}
+                            meterCount={512}
+                            meterColor={[
+                                { stop: 0, color: "#3D58F4" },
+                                { stop: 0.5, color: "#883CD0" },
+                                { stop: 1, color: "#D924AC" },
+                            ]}
+                            gap={4}
+                        />
+                    </div>
+                );
+            } else {
+                vizualizer = (
+                    <div className={classes.Vizualizer}>
+                        <img src={equalizer} alt="Equalizer" />
+                    </div>
+                );
+            }
+        }
+        return (
+            <div className={classes.Track}>
+                {vizualizer}
+                <AudioPlayer
+                    customAdditionalControls={[]}
+                    showJumpControls={false}
+                    src={track}
+                    onEnded={onTrackEnded}
+                    ref={this.player}
+                    onCanPlay={this.onAudioCanPlay}
+                    crossOrigin="anonymous"
+                />
+            </div>
+        );
     }
-    return (
-      <div className={classes.Track}>
-        {vizualizer}
-        <AudioPlayer
-          customAdditionalControls={[]}
-          showJumpControls={false}
-          src={track}
-          onEnded={onTrackEnded}
-          ref={this.player}
-          onCanPlay={this.onAudioCanPlay}
-          crossOrigin="anonymous"
-        />
-      </div>
-    );
-  }
 }
 
 export default Track;
